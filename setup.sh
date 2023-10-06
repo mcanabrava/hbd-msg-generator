9eca559db55f7ad8ff41e74db05b863b05060566
mcanabrava <mcanabrava@github.com>
2023-10-06 09:32:57 -0300

    fix setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[generaç]\n\
-email = \"mcanabrava.andrade@gmail.com"\"n\
+[general]\n\
+email = \"mcanabrava.andrade@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
-headless= true\n\
-enableCORS=false\n\
+headless = true\n\
+enableCORS = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
